366fdcad57311aca6c39daef6af743f13bec2c5f
Ahmet Yusuf TOĞTAY <yusuf.togtay@gmail.com>
2021-06-18 15:40:12 +0300

    inital commit

---
+++ src/main/java/com/paralelsirket/muhabbet/MuhabbetApplication.java
@@ -0,0 +1,13 @@
+package com.paralelsirket.muhabbet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuhabbetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuhabbetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paralelsirket/muhabbet/MuhabbetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paralelsirket.muhabbet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuhabbetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
